93d139127e6b3e12f43af620d75d17bfc8d00c29
Evis44 <87716082+Evis44@users.noreply.github.com>
2021-07-20 23:23:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,37 @@
-rom telegram.ext import Updater, MessageHandler,Filters
+from telegram.ext import Updater, MessageHandler,Filters
 
 from Adafruit_IO import Client
 import os
 
-aio = Client('nostalgicboy22',os.getenv('nostalgicboy22'))
+aio = Client('evis',os.getenv('evis'))
              
 def demo1(bot,update):
   chat_id = bot.message.chat_id
-  path = 'https://image.shutterstock.com/image-vector/ok-hand-lettering-handmade-calligraphy-260nw-669965602.jpg'
-  bot.message.reply_text('I am fine')
-  update.bot.sendPhoto(chat_id=chat_id,photo=path)
-def demo2(bot,update):
-  chat_id = bot.message.chat_id
-  path = 'https://ak.picdn.net/shutterstock/videos/1036409747/thumb/1.jpg'
+  path = 'https://i.pinimg.com/originals/3f/47/e0/3f47e02d05dc601d867771271a70c805.jpg'
   bot.message.reply_text('LIGHT is turned ON')
   aio.send('light', 1)
   data1 = aio.receive('light')
   print(f'Received value: {data1.value}')
   update.bot.sendPhoto(chat_id=chat_id,photo=path)
-def demo3(bot,update):
+def demo2(bot,update):
   chat_id = bot.message.chat_id
-  path = 'https://ak.picdn.net/shutterstock/videos/16051507/thumb/1.jpg'
+  path = 'https://cdn5.vectorstock.com/i/1000x1000/70/44/3d-realistic-off-light-bulb-icon-closeup-vector-27407044.jpg'
   bot.message.reply_text('LIGHT is turned OFF')
   aio.send('light', 0)
   data1 = aio.receive('light')
   print(f'Received value: {data1.value}')
   update.bot.sendPhoto(chat_id=chat_id,photo=path)
-def demo4(bot,update):
+def demo3(bot,update):
   chat_id = bot.message.chat_id
-  path = 'https://cdn.imgbin.com/4/14/11/imgbin-fan-cartoon-green-simple-fan-decoration-pattern-wb2X4GAMBapYZ5w3Te8g1bknd.jpg'
+  path = 'https://thumbs.dreamstime.com/z/vintage-electric-fan-14689784.jpg'
   bot.message.reply_text('FAN is turned ON')
   aio.send('fan', 1)
   data2 = aio.receive('fan')
   print(f'Received value: {data2.value}')
   update.bot.sendPhoto(chat_id=chat_id,photo=path)
-def demo5(bot,update):
+def demo4(bot,update):
   chat_id = bot.message.chat_id
-  path = 'https://previews.123rf.com/images/tawesit/tawesit1601/tawesit160100054/50820091-house-item-electric-fan-cartoon.jpg'
+  path = 'https://c8.alamy.com/comp/DYE942/an-old-green-desk-fan-with-red-ribbons-against-a-white-background-DYE942.jpg'
   bot.message.reply_text('FAN is turned OFF')
   aio.send('fan', 0)
   data2 = aio.receive('fan')
@@ -46,16 +41,14 @@ def main(bot,update):
   a = bot.message.text.lower()
   print(a)
 
-  if a == "how are you?":
+  if a =="lights on" or a=="turn on the light":
     demo1(bot,update)
-  elif a =="light on" or a=="turn on light":
+  elif a =="lights off" or a=="turn off the light":
     demo2(bot,update)
-  elif a =="light off" or a=="turn off light":
-    demo3(bot,update)
   elif a =="switch on the fan" or a=="turn on fan":
-    demo4(bot,update)
+    demo3(bot,update)
   elif a =="switch of the fan" or a=="turn off fan":
-    demo5(bot,update)
+    demo4(bot,update)
   else:
     bot.message.reply_text('Invalid Text')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
